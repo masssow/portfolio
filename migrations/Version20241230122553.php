bfc1d529256aab912d25e95bb415aa1be599ccc9
Samba SOW <masssowcode@gmail.com>
2024-12-31 18:28:57 +0100

    Suppresion3 migrations en conflit avec le Workflow

--- migrations/Version20241230122553.php
+++ migrations/Version20241230122553.php
@@ -19,29 +19,10 @@ final class Version20241230122553 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE admin (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, roles JSON NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_IDENTIFIER_USERNAME (username), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE categorie_posts (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) DEFAULT NULL, description LONGTEXT DEFAULT NULL, image_name VARCHAR(255) DEFAULT NULL, image_size INT DEFAULT NULL, updated_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE message (id INT AUTO_INCREMENT NOT NULL, post_id INT DEFAULT NULL, name VARCHAR(255) DEFAULT NULL, email VARCHAR(255) NOT NULL, content LONGTEXT NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_B6BD307F4B89032C (post_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE posts (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) DEFAULT NULL, content LONGTEXT DEFAULT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', image_name VARCHAR(255) DEFAULT NULL, image_size INT DEFAULT NULL, image_two_name VARCHAR(255) DEFAULT NULL, image_two_size INT DEFAULT NULL, image_three_name VARCHAR(255) DEFAULT NULL, image_three_size INT DEFAULT NULL, paragraphe2 LONGTEXT DEFAULT NULL, paragraphe3 LONGTEXT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE posts_categorie_posts (posts_id INT NOT NULL, categorie_posts_id INT NOT NULL, INDEX IDX_F86135C7D5E258C5 (posts_id), INDEX IDX_F86135C7ED6519AF (categorie_posts_id), PRIMARY KEY(posts_id, categorie_posts_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE message ADD CONSTRAINT FK_B6BD307F4B89032C FOREIGN KEY (post_id) REFERENCES posts (id)');
-        $this->addSql('ALTER TABLE posts_categorie_posts ADD CONSTRAINT FK_F86135C7D5E258C5 FOREIGN KEY (posts_id) REFERENCES posts (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE posts_categorie_posts ADD CONSTRAINT FK_F86135C7ED6519AF FOREIGN KEY (categorie_posts_id) REFERENCES categorie_posts (id) ON DELETE CASCADE');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE message DROP FOREIGN KEY FK_B6BD307F4B89032C');
-        $this->addSql('ALTER TABLE posts_categorie_posts DROP FOREIGN KEY FK_F86135C7D5E258C5');
-        $this->addSql('ALTER TABLE posts_categorie_posts DROP FOREIGN KEY FK_F86135C7ED6519AF');
-        $this->addSql('DROP TABLE admin');
-        $this->addSql('DROP TABLE categorie_posts');
-        $this->addSql('DROP TABLE message');
-        $this->addSql('DROP TABLE posts');
-        $this->addSql('DROP TABLE posts_categorie_posts');
-        $this->addSql('DROP TABLE messenger_messages');
+        
     }
 }
